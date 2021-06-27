e29b7d2daa2ffd8a93f696068b4b89d815aad89d
André Marques <andrepestanamarques@gmail.com>
2021-06-27 13:38:35 +0100

    Addicionar Jogador + Adicionar Clube

---
+++ DIAS_TrabalhoFinalFXML/DiasTrabalhoFinal/src/Controllers/AdicionarClubeController.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import Models.Clubes;
+import diastrabalhofinal.InteracaoBD; 
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.net.URL;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author André Marques
+ */
+public class AdicionarClubeController implements Initializable {
+
+    @FXML
+    private TextField txtNomeClube;
+    @FXML
+    private TextField txtPais;
+    @FXML
+    private TextField txtCodClube;
+    @FXML
+    private Button btnAdicionar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void addClube(ActionEvent event) throws SQLException, ParseException {
+
+           Clubes clubes = new Clubes(Integer.parseInt(txtCodClube.getText()), txtNomeClube.getText(), txtPais.getText());
+
+                connect.adicionarJogo(clubes);
+                ((Node) (event.getSource())).getScene().getWindow().hide();
+                
+                System.out.println("Registo inserido com sucesso!");
+                
+    }
+   
+
+  
+     public void abrirJanela(String caminho, ActionEvent event1) throws IOException {
+        FXMLLoader fxml = new FXMLLoader(getClass().getResource(caminho));
+        Parent root = (Parent) fxml.load();
+        Stage stage = new Stage();
+        stage.setTitle("Rato Esquilo - Gestão da Superliga");
+        stage.setScene(new Scene(root, 829, 550));
+        stage.setResizable(false);
+        stage.show();
+
+    }
+
+}
